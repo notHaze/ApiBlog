0bbeb55c80f552786cf3c7e9a2a685ec6f0184a4
Florent Combet <florent.combet@etu.iut-tlse3.fr>
2023-03-16 09:05:48 +0100

    Add token auth middelware for /article route

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ use Infrastructure\SynchronousQueryBus;
 use Application\Query\User\FindIdentityQueryHandler;
 use Domain\Repository\LoginRepositoryImpl;
 use Application\Command\User\ValidateTokenCommandHandler;
+use Application\Middleware\ValidateTokenMiddleware;
 use Application\Query\User\GetRoleQueryHandler;
 use Application\Query\User\GetTokenQueryHandler;
 
@@ -38,7 +39,14 @@ SynchronousQueryBus::register(Application\Query\User\GetTokenQuery::class, new G
 
 
 $app->post('/login', [LoginController::class, 'login']);
-$app->get('/article', [ArticleController::class, 'getAll']);
+
+$app->group('/article', function ($app) {
+    $app->get('/{id}', [ArticleController::class, 'get']);
+    $app->post('', [ArticleController::class, 'create']);
+    $app->patch('/{id}', [ArticleController::class, 'update']);
+    $app->delete('/{id}', [ArticleController::class, 'delete']);
+    $app->get('', [ArticleController::class, 'getAll']);
+})->addMiddleware(new ValidateTokenMiddleware($app->getResponseFactory()));
 
 try {
     $app->run();     
---
+++ src/Domain/tokenJWT/Exception/TokenNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Domain\tokenJWT\Exception;
+
+use Exception;
+
+class TokenNotFoundException extends Exception
+{
+
+}
